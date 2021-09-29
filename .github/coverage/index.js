75868abdf2c17f7a38731c58640df382cd0bc364
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-29 14:56:50 +0200

    codecov: regenerate report

---
+++ .github/coverage/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"run_coverage.6a19474a6736a49f/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"93.5","covered_lines":"116","uncovered_lines":"8","total_lines" : "124"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "run_coverage", "date" : "2021-09-29 14:56:13", "instrumented" : 124, "covered" : 116,};
---
+++ .github/coverage/run_coverage.6a19474a6736a49f/index.js
@@ -0,0 +1,8 @@
+var data = {files:[
+{"link":"rlp.cr.6205360f.html","title":"rlp.cr","summary_name":"[...]/rlp.cr/src/rlp.cr","covered_class":"lineCov","covered":"90.9","covered_lines":"80","uncovered_lines":"8","total_lines" : "88"},
+{"link":"util.cr.a1800184.html","title":"util.cr","summary_name":"[...]/rlp.cr/src/util.cr","covered_class":"lineCov","covered":"100.0","covered_lines":"33","uncovered_lines":"0","total_lines" : "33"},
+{"link":"constants.cr.33bbb6c.html","title":"constants.cr","summary_name":"[...]/rlp.cr/src/constants.cr","covered_class":"lineCov","covered":"100.0","covered_lines":"3","uncovered_lines":"0","total_lines" : "3"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "run_coverage", "date" : "2021-09-29 14:56:13", "instrumented" : 124, "covered" : 116,};
+var merged_data = [];
